7306dd52bd927e79a4c1731654cb477d24e87392
HenryJohnPeters <henryjohnpeters@gmail.com>
2020-09-24 18:09:35 +0100

    Add project files.

---
+++ ExampleSolaceGlobal/Models/LocationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleSolaceGlobal.Models
+{
+    public class LocationData
+    {
+        public string Category { get; set; }
+        public string Risk { get; set; }
+        public float Country { get; set; }
+        public float longitude { get; set; }
+        public string latitude { get; set; }
+      
+
+    }
+}
